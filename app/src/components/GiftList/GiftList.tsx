5706d0856e2eb069724e743503e684349cd6e8cb
florianmg <mesagouiffes@gmail.com>
2024-06-25 16:24:08 +0200

    fix: add key prop to Card component in GiftList

--- app/src/components/GiftList/GiftList.tsx
+++ app/src/components/GiftList/GiftList.tsx
@@ -6,7 +6,7 @@ export const GiftList = ({ gifts }: GiftListProps) => {
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
       {gifts.map((gift) => (
-        <Card {...gift} />
+        <Card key={gift.id} {...gift} />
       ))}
     </div>
   );
