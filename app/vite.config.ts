3612f4b3740938f8ac5a040028a0ef939f6570c7
florianmg <mesagouiffes@gmail.com>
2024-06-26 09:13:39 +0200

    chore: install svg plugin to easy import

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vitest/config';
+import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
