b7f081607b473efc84d5b350e1b73247b9dd5f9a
florianmg <mesagouiffes@gmail.com>
2024-06-26 11:25:14 +0200

    feat: update ConsumptionCard layout

--- app/src/components/GiftDetails/ConsumptionCard/ConsumptionCard.tsx
+++ app/src/components/GiftDetails/ConsumptionCard/ConsumptionCard.tsx
@@ -7,22 +7,26 @@ export const ConsumptionCard = ({ beneficiaries }: ConsumptionCardProps) => (
     <div className="flex size-8 items-center justify-center rounded-lg bg-green-100">
       <FamilyIcon className="size-3.5 fill-green-800" />
     </div>
-    <p>Quel(s) ayant(s)-droit validés bénéficient de cette cagnotte ?</p>
-    <div className="flex">
-      {beneficiaries.map(({ id, type }) => (
-        <div key={id} className="-ml-2 first:ml-0">
-          <BenificiariesIcon type={type} />
-        </div>
-      ))}
-    </div>
-    <div>
-      {beneficiaries.map(({ id, type, firstName }, index) => (
-        <span key={id} className="text-xs text-slate-600">
-          {type === 'user' ? 'Vous mêmes' : firstName}
-          {index < beneficiaries.length - 2 && ', '}
-          {index === beneficiaries.length - 2 && ' et '}
-        </span>
-      ))}
+    <p className="mt-4">
+      Quel(s) ayant(s)-droit validés bénéficient de cette cagnotte ?
+    </p>
+    <div className="mt-2 flex items-center gap-x-2">
+      <div className="flex">
+        {beneficiaries.map(({ id, type }) => (
+          <div key={id} className="-ml-2 first:ml-0">
+            <BenificiariesIcon type={type} />
+          </div>
+        ))}
+      </div>
+      <div>
+        {beneficiaries.map(({ id, type, firstName }, index) => (
+          <span key={id} className="text-xs text-slate-600">
+            {type === 'user' ? 'Vous mêmes' : firstName}
+            {index < beneficiaries.length - 2 && ', '}
+            {index === beneficiaries.length - 2 && ' et '}
+          </span>
+        ))}
+      </div>
     </div>
   </div>
 );
