cbf67d62d97274e2cc1d8c408223f1f2696b6ba2
florianmg <mesagouiffes@gmail.com>
2024-06-26 15:34:02 +0200

    fix: use env variables for api endpoint

--- app/src/hooks/queries/giftCardDetails/useGiftCardDetails.ts
+++ app/src/hooks/queries/giftCardDetails/useGiftCardDetails.ts
@@ -3,7 +3,9 @@ import { useQuery } from '@tanstack/react-query';
 import { giftCardDetailsQueryKeys } from './queryKeys';
 
 const fetchGiftCardDetails = async (id: string): Promise<Gift> => {
-  const response = await fetch(`http://localhost:3001/gift-cards/${id}`);
+  const response = await fetch(
+    `${import.meta.env.VITE_API_ENDPOINT}/gift-cards/${id}`
+  );
   const data = await response.json();
   if (response.status !== 200) {
     throw data;
--- app/src/hooks/queries/giftsCards/useGiftCards.ts
+++ app/src/hooks/queries/giftsCards/useGiftCards.ts
@@ -3,7 +3,9 @@ import { useQuery } from '@tanstack/react-query';
 import { giftCardsQueryKeys } from './queryKeys';
 
 const fetchGiftCards = async (): Promise<Gift[]> => {
-  const response = await fetch(`http://localhost:3001/gift-cards`);
+  const response = await fetch(
+    `${import.meta.env.VITE_API_ENDPOINT}/gift-cards`
+  );
   const data = await response.json();
   if (response.status !== 200) {
     throw data;
